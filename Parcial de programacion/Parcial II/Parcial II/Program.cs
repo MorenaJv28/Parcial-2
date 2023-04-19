f39ef7acf264c98f7c556a7a63279a90595446ee
MorenaJv28 <124007217+MorenaJv28@users.noreply.github.com>
2023-04-18 21:29:17 -0600

    Add files via upload

---
+++ Parcial de programacion/Parcial II/Parcial II/Program.cs
@@ -0,0 +1,11 @@
+﻿int dat1 = 16;
+String dat2 = "25g";
+try
+{
+    int resultado = dat1 + Convert.ToInt32(dat2);
+    Console.WriteLine(resultado);
+}
+catch (Exception)
+{
+    Console.WriteLine("No se pudo realizar la operación solicitada");
+}
